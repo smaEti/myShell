3288a7d42d9bae1f7a9f74834a609fff6dba0c37
Amin Ehterami <A.Ehterami@proton.me>
2025-12-02 14:22:53 +0330

    Add command not found error

--- app/main.go
+++ app/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
 	fmt.Print("$ ")
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+
+	if err != nil {
+		fmt.Println("An error is eccoured")
+	}
+
+	fmt.Println(command[:len(command)-1] + ": command not found")
 }
