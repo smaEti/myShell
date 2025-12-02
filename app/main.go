42d79e56a0839ab519936f81ae492c5ad9cb2819
Amin Ehterami <A.Ehterami@proton.me>
2025-12-02 14:36:31 +0330

    Add REPL

--- app/main.go
+++ app/main.go
@@ -7,12 +7,15 @@ import (
 )
 
 func main() {
-	fmt.Print("$ ")
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	for true {
 
-	if err != nil {
-		fmt.Println("An error is eccoured")
+		fmt.Print("$ ")
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+
+		if err != nil {
+			fmt.Println("An error is eccoured")
+		}
+
+		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
-
-	fmt.Println(command[:len(command)-1] + ": command not found")
 }
