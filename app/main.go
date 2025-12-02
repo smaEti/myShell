2ac1713b68395e99a751d6b073bcbbca8d8ece29
Amin Ehterami <A.Ehterami@proton.me>
2025-12-02 14:57:45 +0330

    Implement echo

--- app/main.go
+++ app/main.go
@@ -4,22 +4,34 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	for true {
 
 		fmt.Print("$ ")
+		commandArgs, command := readingCommand()
 
-		commandWithEndLine, err := bufio.NewReader(os.Stdin).ReadString('\n')
-		command := commandWithEndLine[:len(commandWithEndLine)-1]
-		if err != nil {
-			fmt.Println("An error is eccoured")
+		if commandArgs[0] == "echo" {
+			fmt.Println(command[4:])
+			continue
 		}
-
 		if command == "exit" {
 			os.Exit(0)
 		}
 		fmt.Println(command + ": command not found")
 	}
+}
+
+func readingCommand() ([]string, string) {
+	commandWithEndLine, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	command := commandWithEndLine[:len(commandWithEndLine)-1]
+	if err != nil {
+		fmt.Println("An error is eccoured")
+		os.Exit(2)
+	}
+	command = strings.TrimSpace(command)
+	commandFields := strings.Fields(command)
+	return commandFields, command
 }
