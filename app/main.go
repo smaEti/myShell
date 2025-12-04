dba90782e244e4313895d28f602b0b5256d50cef
Amin Ehterami <A.Ehterami@proton.me>
2025-12-04 14:05:17 +0330

    refactor: use stdin,out,error instead of printing text (for direction, pipe,... in the future)

--- app/main.go
+++ app/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -14,31 +15,157 @@ import (
 
 var builtIn = []string{"type", "echo", "exit", "pwd", "cd"}
 
-func main() {
+type executableCommand struct {
+	Stdout io.Writer
+	Stderr io.Writer
+	Stdin  io.Reader
+	Name   string
+	Args   []string
+}
 
+func main() {
 	for {
 		fmt.Print("$ ")
 		commandArgs := readingCommand()
-		if slices.Contains(builtIn, commandArgs[0]) {
-			handleBuiltInCommands(commandArgs)
+
+		if len(commandArgs) == 0 {
 			continue
 		}
 
-		executableFile := findExecutable(commandArgs[0])
-		if executableFile != "" {
-			cmd := exec.Command(commandArgs[0], commandArgs[1:]...)
-
-			cmd.Stdout = os.Stdout
-			cmd.Stderr = os.Stderr
-			cmd.Stdin = os.Stdin
-			cmd.Run()
-			continue
-		} else {
-			fmt.Println(commandArgs[0] + ": command not found")
-			continue
+		cmd := &executableCommand{
+			Stdout: os.Stdout,
+			Stderr: os.Stderr,
+			Stdin:  os.Stdin,
+			Name:   commandArgs[0],
+			Args:   commandArgs,
 		}
+
+		cmd.Execute()
 	}
 }
+
+// Execute determines whether to run a built-in or external command
+func (cmd *executableCommand) Execute() error {
+	// Handle built-in commands
+	if slices.Contains(builtIn, cmd.Name) {
+		return cmd.executeBuiltIn()
+	}
+
+	// Handle external executables
+	executableFile := findExecutable(cmd.Name)
+	if executableFile != "" {
+		return cmd.executeExternal()
+	}
+
+	// Command not found
+	fmt.Fprintln(cmd.Stderr, cmd.Name+": command not found")
+	return fmt.Errorf("command not found: %s", cmd.Name)
+}
+
+// executeBuiltIn handles built-in commands
+func (cmd *executableCommand) executeBuiltIn() error {
+	switch cmd.Name {
+	case "exit":
+		return cmd.handleExit()
+	case "cd":
+		return cmd.handleCd()
+	case "pwd":
+		return cmd.handlePwd()
+	case "type":
+		return cmd.handleType()
+	case "echo":
+		return cmd.handleEcho()
+	default:
+		return fmt.Errorf("unknown built-in: %s", cmd.Name)
+	}
+}
+
+// executeExternal handles external executable commands
+func (cmd *executableCommand) executeExternal() error {
+	var args []string
+	if len(cmd.Args) > 1 {
+		args = cmd.Args[1:]
+	}
+
+	externalCmd := exec.Command(cmd.Name, args...)
+	externalCmd.Stdout = cmd.Stdout
+	externalCmd.Stderr = cmd.Stderr
+	externalCmd.Stdin = cmd.Stdin
+
+	return externalCmd.Run()
+}
+
+// Built-in command handlers
+func (cmd *executableCommand) handleExit() error {
+	os.Exit(0)
+	return nil
+}
+
+func (cmd *executableCommand) handleCd() error {
+	if len(cmd.Args) < 2 {
+		fmt.Fprintln(cmd.Stderr, "cd: missing argument")
+		return fmt.Errorf("missing argument")
+	}
+
+	path := cmd.Args[1]
+	if path == "~" {
+		path = os.Getenv("HOME")
+	}
+
+	err := os.Chdir(path)
+	if err != nil {
+		fmt.Fprintln(cmd.Stderr, "cd: "+cmd.Args[1]+": No such file or directory")
+		return err
+	}
+	return nil
+}
+
+func (cmd *executableCommand) handlePwd() error {
+	pwd, err := os.Getwd()
+	if err != nil {
+		fmt.Fprintln(cmd.Stderr, "An error occurred in reading the directory:", err)
+		return err
+	}
+	fmt.Fprintln(cmd.Stdout, pwd)
+	return nil
+}
+
+func (cmd *executableCommand) handleType() error {
+	if len(cmd.Args) < 2 {
+		fmt.Fprintln(cmd.Stderr, "type: missing argument")
+		return fmt.Errorf("missing argument")
+	}
+
+	command := cmd.Args[1]
+
+	if slices.Contains(builtIn, command) {
+		fmt.Fprintln(cmd.Stdout, command+" is a shell builtin")
+		return nil
+	}
+
+	file := findExecutable(command)
+	if file != "" {
+		fmt.Fprintln(cmd.Stdout, command+" is "+file)
+		return nil
+	}
+
+	fmt.Fprintln(cmd.Stdout, command+": not found")
+	return nil
+}
+
+func (cmd *executableCommand) handleEcho() error {
+	if len(cmd.Args) > 1 {
+		for i, arg := range cmd.Args[1:] {
+			if i > 0 {
+				fmt.Fprint(cmd.Stdout, " ")
+			}
+			fmt.Fprint(cmd.Stdout, arg)
+		}
+	}
+	fmt.Fprintln(cmd.Stdout)
+	return nil
+}
+
 func readingCommand() []string {
 	reader := bufio.NewReader(os.Stdin)
 	commandWithEndLine, err := reader.ReadString('\n')
@@ -46,9 +173,7 @@ func readingCommand() []string {
 		fmt.Println("An error occurred while reading the command line:", err)
 		os.Exit(2)
 	}
-
 	command := strings.TrimSpace(commandWithEndLine)
-
 	inQuotes := false
 	var quoteChar rune
 	var sb strings.Builder
@@ -57,10 +182,8 @@ func readingCommand() []string {
 	for i := 0; i < len(command); i++ {
 		r := rune(command[i])
 
-		// Handle escape characters
 		if r == '\\' && i+1 < len(command) {
 			next := rune(command[i+1])
-
 			if inQuotes && quoteChar == '"' {
 				if next == '\\' || next == '"' {
 					sb.WriteRune(next)
@@ -79,20 +202,18 @@ func readingCommand() []string {
 			}
 		}
 
-		// Handle quoting logic
 		switch {
 		case r == '\'' || r == '"':
 			if inQuotes {
 				if r == quoteChar {
-					inQuotes = false // closing quote
+					inQuotes = false
 				} else {
-					sb.WriteRune(r) // quote inside other type of quotes
+					sb.WriteRune(r)
 				}
 			} else {
 				inQuotes = true
 				quoteChar = r
 			}
-
 		case unicode.IsSpace(r):
 			if inQuotes {
 				sb.WriteRune(r)
@@ -100,7 +221,6 @@ func readingCommand() []string {
 				result = append(result, sb.String())
 				sb.Reset()
 			}
-
 		default:
 			sb.WriteRune(r)
 		}
@@ -113,53 +233,32 @@ func readingCommand() []string {
 	return result
 }
 
-func handleTypeCommand(command string) {
-	if slices.Contains(builtIn, command) {
-		fmt.Println(command + " is a shell builtin")
-		return
-	}
-	//locate executables
-	file := findExecutable(command)
-	if file != "" {
-		fmt.Println(command + " is " + file)
-		return
-	}
-	fmt.Println(command + ": not found")
-}
-
 func isExecutable(path string) bool {
 	info, err := os.Stat(path)
 	if err != nil {
 		return false
 	}
-
 	if info.IsDir() {
 		return false
 	}
-
 	mode := info.Mode()
-
 	if mode&0111 != 0 {
 		return true
 	}
-
 	return false
 }
 
 func findExecutable(command string) string {
 	pathString := os.Getenv("PATH")
 	paths := strings.Split(pathString, string(os.PathListSeparator))
-
 	for _, path := range paths {
 		files, err := os.ReadDir(path)
 		if err != nil {
 			continue
 		}
-
 		index := sort.Search(len(files), func(i int) bool {
 			return files[i].Name() >= command
 		})
-
 		if index < len(files) && files[index].Name() == command {
 			fullPath := filepath.Join(path, command)
 			isFileExecutable := isExecutable(fullPath)
@@ -169,48 +268,4 @@ func findExecutable(command string) string {
 		}
 	}
 	return ""
-}
-func handlePwdCommand() {
-	pwd, err := os.Getwd()
-	if err != nil {
-		fmt.Println("An error is eccoured in reading the commandline", err)
-		os.Exit(2)
-	}
-	fmt.Println(pwd)
-}
-func handleCdCommand(commandArgs []string) {
-	if commandArgs[1] == "~" {
-		commandArgs[1] = os.Getenv("HOME")
-	}
-	err := os.Chdir(commandArgs[1])
-	if err != nil {
-		fmt.Println("cd: " + commandArgs[1] + ": No such file or directory")
-	}
-}
-func handleEchoCommand(commandArgs []string) {
-	for _, arg := range commandArgs {
-		fmt.Print(arg + " ")
-	}
-	fmt.Println()
-
-}
-func handleBuiltInCommands(commandArgs []string) {
-	if commandArgs[0] == "cd" {
-		handleCdCommand(commandArgs)
-	}
-	if commandArgs[0] == "pwd" {
-		handlePwdCommand()
-	}
-
-	if commandArgs[0] == "type" {
-		handleTypeCommand(commandArgs[1])
-	}
-
-	if commandArgs[0] == "echo" {
-		handleEchoCommand(commandArgs[1:])
-	}
-
-	if commandArgs[0] == "exit" {
-		os.Exit(0)
-	}
 }
